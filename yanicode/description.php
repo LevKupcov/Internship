25faf7890532de28c48066581acfd62ae823d73a
Akira <120242344+LevKupcov@users.noreply.github.com>
2025-11-03 10:08:09 +0200

    1

--- yanicode/description.php
+++
@@ -1,7 +0,0 @@
-<?
-$arTemplate = array (
-  'NAME' => 'LO2 - Yanicode Design',
-  'DESCRIPTION' => 'Шаблон с дизайном Yanicode (хедер и футер как в www/local)',
-  'SORT' => '',
-  'TYPE' => '',
-);
